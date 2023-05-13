d65b7b36d81f48f4dd51d1bf0e0a4a9dc691d21f
LeopoldoMAW <leopoldo.mendez.sosa@awsoftware.com.ar>
2023-05-13 17:19:59 -0300

    Se agregan funciones auxiliares

--- iap1-tp.hs
+++ iap1-tp.hs
@@ -41,6 +41,55 @@ pertenece _ [] = False
 pertenece t (x:xs)
     | t == x = True
     | otherwise = pertenece t xs
+    
+------------------------------------------Leo------------------------------------------
+
+{- Evalua, dada una lista de Usuarios y una lista de Publicaciones, las funciones: 
+usuariosDePublicacionSonUsuariosDeRed, usuariosDeLikeDePublicacionSonUsuariosDeRed, noHayPublicacionesRepetidas
+retornen True,en dicho caso, la funcion retorna True, en caso de que alguna de ellas retorne False, la funcion publicacionValidas retorna False -}
+publicacionesValidas :: [Usuario] -> [Publicacion] -> Bool
+publicacionesValidas users pubs
+    |   usuariosDePublicacionSonUsuariosDeRed users pubs && usuariosDeLikeDePublicacionSonUsuariosDeRed users pubs && noHayPublicacionesRepetidas pubs = True
+    |   otherwise = False
+
+{- Evalua que, dada dos listas una de usuarios y otra de publicaciones, los usuarios de la lista de publicaciones pertenezcan a la lista de Usuarios,
+en cuyo caso, la funcion retorna True, en caso contrario False -}
+usuariosDePublicacionSonUsuariosDeRed :: [Usuario] -> [Publicacion] -> Bool
+usuariosDePublicacionSonUsuariosDeRed _ [] = True
+usuariosDePublicacionSonUsuariosDeRed users (pub:pubs)
+    |   pertenece (usuarioDePublicacion pub) users = usuariosDePublicacionSonUsuariosDeRed users pubs
+    |   otherwise = False
+
+{- Evalua que, dada dos listas una de usuarios y otra de publicaciones, los likes de la lista de publicaciones, sean dados por usuarios de la lista de Usuarios
+en cuyo caso, la funcion retorna True, en caso contrario False -}
+usuariosDeLikeDePublicacionSonUsuariosDeRed :: [Usuario] -> [Publicacion] -> Bool
+usuariosDeLikeDePublicacionSonUsuariosDeRed _ [] = True
+usuariosDeLikeDePublicacionSonUsuariosDeRed users (pub:pubs)
+    |   usuariosLikeValidos users (likesDePublicacion pub) = usuariosDeLikeDePublicacionSonUsuariosDeRed users pubs
+    |   otherwise = False
+
+{- Evalua que, dada dos listas de Usuarios, todos los usuarios de la segunda lista, pertenezcan a la primera.
+En este caso la funcion retorna True, en caso de que alguno no pertenezca, retorna False -}
+usuariosLikeValidos :: [Usuario] -> [Usuario] -> Bool
+usuariosLikeValidos _ [] = True
+usuariosLikeValidos users (userL:userLs)
+    |   pertenece userL users = usuariosLikeValidos users userLs
+    |   otherwise = False
+
+{- Evalua que no existan publicaciones repetidas en una misma lista de publicaciones
+en caso de existir publicaciones iguales retorna False, caso de no encontrar ninguna, retorna True -}
+noHayPublicacionesRepetidas :: [Publicacion] -> Bool
+noHayPublicacionesRepetidas [] = True
+noHayPublicacionesRepetidas [pub] = True
+noHayPublicacionesRepetidas (pub:pubs) 
+    | pertenece pub pubs = False
+    | otherwise = noHayPublicacionesRepetidas pubs
+
+--Evalua que la lista dada comience con el valor indicado, en cuyo caso devuelve True, caso contrario devuelve False    
+empiezaCon :: (Eq x) => x -> [x] -> Bool
+empiezaCon t xs
+    |head xs == t = True
+    |otherwise = False
 
 -- Ejercicios
 
@@ -82,3 +131,5 @@ tieneUnSeguidorFiel = undefined
 -- describir qué hace la función: .....
 existeSecuenciaDeAmigos :: RedSocial -> Usuario -> Usuario -> Bool
 existeSecuenciaDeAmigos = undefined
+
+
