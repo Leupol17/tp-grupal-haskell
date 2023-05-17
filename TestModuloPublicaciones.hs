21d6f8e667bcde3497ea16c4561e05977f043d60
Matias-Romano <matias.romano.whaley@gmail.com>
2023-05-16 23:17:29 -0300

    -Ejercicio 7 y 8

---
+++ TestModuloPublicaciones.hs
@@ -0,0 +1,52 @@
+module TestModuloPublicaciones where
+
+import Test.HUnit
+import ModuloPublicaciones
+
+main = runTestTT tests
+
+tests = test [testsuite1, testsuite2]
+
+testsuite1 = test [
+
+
+
+]
+
+testsuite1 = test [
+
+    
+
+]
+
+usuario1 = (1, "Juan")
+usuario2 = (2, "Natalia")
+usuario3 = (3, "Pedro")
+usuario4 = (4, "Mariela")
+usuario5 = (5, "Natalia")
+
+publicacion1_1 = (usuario1, "Este es mi primer post", [usuario2, usuario4])
+publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
+publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
+publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
+publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
+
+publicacion2_1 = (usuario2, "Hello World", [usuario4])
+publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
+
+publicacion3_1 = (usuario3, "Lorem Ipsum", [])
+publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
+publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
+
+publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
+publicacion4_2 = (usuario4, "I am Bob", [])
+publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
+
+usuariosA = [usuario1, usuario2, usuario3, usuario4]
+usuariosB = [usuario1, usuario2, usuario3, usuario5]
+
+publicacionesA = [publicacion1_1, publicacion1_2, publicacion2_1, publicacion2_2, publicacion3_1, publicacion3_2, publicacion4_1, publicacion4_2]
+publicacionesB = [publicacion1_3, publicacion1_4, publicacion1_5, publicacion3_1, publicacion3_2, publicacion3_3]
+
+redA = (usuariosA, [], publicacionesA)
+redB = (usuariosB, [], publicacionesB)
