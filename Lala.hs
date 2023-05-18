84ce1aa55e0e8bd78426f8c4bd8d2cf130b96050
sebastian <troncoso.seb.99@gmail.com>
2023-05-18 12:57:20 -0300

    a

--- Lala.hs
+++ Lala.hs
@@ -2,7 +2,6 @@ module Lala where
 
 import Solucion
 
-
 usuario1 = (1, "Juan")
 usuario2 = (2, "Natalia")
 usuario3 = (3, "Pedro")
@@ -59,6 +58,7 @@ publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
 publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
 publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
 publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
+publicacion1_6 = (usuario1, "Este es como mi sexto post", [usuario2,usuario3])
 
 publicacion2_1 = (usuario2, "Hello World", [usuario4])
 publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
@@ -66,53 +66,20 @@ publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
 publicacion3_1 = (usuario3, "Lorem Ipsum", [])
 publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
 publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
+publicacion3_4 = (usuario3, "consectetur adipiscing elit bis", [usuario2, usuario1])
 
 publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
 publicacion4_2 = (usuario4, "I am Bob", [])
 publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
 
 
+
 publicacion1_2_3 = (usuario1,"hola a todos",[usuario2,usuario3]) --publica usuario 1, dan mg usuario2 y 3
 
-redSocial1 = ([usuario1, usuario2], [relacion1_2, relacion2_1], [publicacion1_2_3])
+redSocial1 = ([usuario1,usuario2,usuario3], [relacion1_2,relacion1_3,relacion1_4], [])
 redSocial2 = ([usuario1, usuario2], [relacion1_2, relacion2_1], [publicacion1_2_3])
 redSocial3 = ([usuario1], [], [])
 redSocial4 = ([usuario1], [], [publicacion1_0_1])
 redSocial5 = ([usuario1], [], [publicacion1_0_1,publicacion1_0_2,publicacion1_0_3])
-
-
-
-
-
-
--- Definición de usuarios, relaciones y publicaciones de ejemplo
-usuariosEjemplo :: [Usuario]
-usuariosEjemplo = [(1, "usuario1"), (2, "usuario2"), (3, "usuario3"), (4, "usuario4")]
-
-relacionesEjemplo :: [Relacion]
-relacionesEjemplo = [((1, "usuario1"), (2, "usuario2")), ((2, "usuario2"), (3, "usuario3")), ((3, "usuario3"), (1, "usuario1"))]
-
-publicacionesEjemplo :: [Publicacion]
-publicacionesEjemplo = [((1, "usuario1"), "Publicación 1", [(2, "usuario2"), (3, "usuario3")])
-                       , ((2, "usuario2"), "Publicación 2", [(1, "usuario1"), (3, "usuario3")])
-                       , ((3, "usuario3"), "Publicación 3", [(1, "usuario1"), (2, "usuario2")])
-                       , ((4, "usuario4"), "Publicación 4", [])
-                       ]
-
-redSocialEjemplo :: RedSocial
-redSocialEjemplo = (usuariosEjemplo, relacionesEjemplo, publicacionesEjemplo)
-
--- Prueba de la función tieneUnSeguidorFiel
-ejemploSeguidorFiel :: Bool
-ejemploSeguidorFiel = tieneUnSeguidorFiel redSocialEjemplo (1, "usuario1")
--- Valor esperado: True, ya que el usuario (1, "usuario1") tiene al menos un seguidor fiel
-
--- Prueba de la función tieneUnSeguidorFiel con otro usuario
-ejemploSeguidorFiel2 :: Bool
-ejemploSeguidorFiel2 = tieneUnSeguidorFiel redSocialEjemplo (2, "usuario2")
--- Valor esperado: True, ya que el usuario (2, "usuario2") tiene al menos un seguidor fiel
-
--- Prueba de la función tieneUnSeguidorFiel con un usuario sin seguidores fieles
-ejemploSeguidorFiel3 :: Bool
-ejemploSeguidorFiel3 = tieneUnSeguidorFiel redSocialEjemplo (4, "usuario4")
--- Valor esperado: False, ya que el usuario (4, "usuario4") no tiene seguidores fieles
+redSocial6 = ([usuario1,usuario2,usuario3], [relacion1_2,relacion1_3,relacion2_3], [publicacion3_4,publicacion1_5,publicacion2_2])
+redSocial7 = ([usuario1,usuario2,usuario3], [relacion1_2,relacion1_3,relacion2_3], [publicacion3_4,publicacion3_3,publicacion1_5,publicacion1_4,publicacion2_2,publicacion2_1])
--- Tests6y9.hs
+++ Tests6y9.hs
@@ -8,11 +8,12 @@ main = runTestTT tests
 tests = test [
         "publicacionesDe tiene 1 usuario sin publicacion " ~:(publicacionesDe redSocial3 usuario1) ~?= [],
         "publicacionesDe tiene 1 usuario con 1 publicacion" ~:(publicacionesDe redSocial4 usuario1) ~?= [((1,"Juan"),"hola a todos",[])],
-        "publicacionesDe tiene 1 usuario con varias publicaciones" ~:(publicacionesDe redSocial5 usuario1) ~?= [((1,"Juan"),"hola a todos",[]),((1,"Juan"),"Que tal",[]),((1,"Juan"),"Como estan",[])]
-        "publicacionesDe tiene varios usuarios sin publicaciones"
-        "publicacionesDe tiene varios usuarios con 1 publicacion"
-        "publicacionesDe tiene varios usuarios con varias publicaciones"
-    ]
+        "publicacionesDe tiene 1 usuario con varias publicaciones" ~:(publicacionesDe redSocial5 usuario1) ~?= [((1,"Juan"),"hola a todos",[]),((1,"Juan"),"Que tal",[]),((1,"Juan"),"Como estan",[])],
+        "publicacionesDe tiene varios usuarios sin publicaciones" ~:(publicacionesDe redSocial1 usuario1 ) ~?= [],
+        "publicacionesDe tiene varios usuarios con 1 publicacion" ~:(publicacionesDe redSocial6 usuario3) ~?= [((3,"Pedro"),"consectetur adipiscing elit bis",[(2,"Natalia"),(1,"Juan")])],
+        "publicacionesDe tiene varios usuarios con varias publicaciones" ~:(publicacionesDe redSocial7 usuario2) ~?= [((2,"Natalia"),"Good Bye World",[(1,"Juan"),(4,"Mariela")]),((2,"Natalia"),"Hello World",[(4,"Mariela")])]
+
+  ]
 
 --testsuite1Seba = test [
   --  "relacionadosDirecto Caso 1: tanto (u1,u2) como (u2,u1) pertenecen a la relacion"~:(relacionadosDirecto usuario1 usuario2 redSocial1 && relacionadosDirecto usuario2 usuario1 redSocial1)~?= True,
@@ -76,6 +77,7 @@ publicacion1_2 = (usuario1, "Este es mi segundo post", [usuario4])
 publicacion1_3 = (usuario1, "Este es mi tercer post", [usuario2, usuario5])
 publicacion1_4 = (usuario1, "Este es mi cuarto post", [])
 publicacion1_5 = (usuario1, "Este es como mi quinto post", [usuario5])
+publicacion1_6 = (usuario1, "Este es como mi sexto post", [usuario2,usuario3])
 
 publicacion2_1 = (usuario2, "Hello World", [usuario4])
 publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
@@ -83,16 +85,21 @@ publicacion2_2 = (usuario2, "Good Bye World", [usuario1, usuario4])
 publicacion3_1 = (usuario3, "Lorem Ipsum", [])
 publicacion3_2 = (usuario3, "dolor sit amet", [usuario2])
 publicacion3_3 = (usuario3, "consectetur adipiscing elit", [usuario2, usuario5])
+publicacion3_4 = (usuario3, "consectetur adipiscing elit bis", [usuario2, usuario1])
 
 publicacion4_1 = (usuario4, "I am Alice. Not", [usuario1, usuario2])
 publicacion4_2 = (usuario4, "I am Bob", [])
 publicacion4_3 = (usuario4, "Just kidding, i am Mariela", [usuario1, usuario3])
 
 
+
 publicacion1_2_3 = (usuario1,"hola a todos",[usuario2,usuario3]) --publica usuario 1, dan mg usuario2 y 3
 
-redSocial1 = ([usuario1, usuario2], [relacion1_2, relacion2_1], [publicacion1_2_3])
+redSocial1 = ([usuario1,usuario2,usuario3], [relacion1_2,relacion1_3,relacion1_4], [])
 redSocial2 = ([usuario1, usuario2], [relacion1_2, relacion2_1], [publicacion1_2_3])
 redSocial3 = ([usuario1], [], [])
 redSocial4 = ([usuario1], [], [publicacion1_0_1])
 redSocial5 = ([usuario1], [], [publicacion1_0_1,publicacion1_0_2,publicacion1_0_3])
+redSocial6 = ([usuario1,usuario2,usuario3], [relacion1_2,relacion1_3,relacion2_3], [publicacion3_4,publicacion1_5,publicacion2_2])
+redSocial7 = ([usuario1,usuario2,usuario3], [relacion1_2,relacion1_3,relacion2_3], [publicacion3_4,publicacion3_3,publicacion1_5,publicacion1_4,publicacion2_2,publicacion2_1])
+redSocial8 = 
